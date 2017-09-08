48f2fa9d53e19148d4c984638a2b4ff8b6fc972f
Max Peiros <mpeiros@gmail.com>
2017-09-08 12:47:38 -0700

    Add skeleton code to users controller spec file

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'POST #create' do
+    context 'when valid params are passed' do
+      before(:each) do |test|
+        post :create, params: { user: { 
+                                  username: 'Example',
+                                  email: 'example@test.com',
+                                  password: 'password'
+                                } 
+                              } unless test.metadata[:has_request]
+      end
 
+      
+    end
+
+    context 'when invalid params are passed' do
+      before(:each) do |test|
+        post :create, params: { user: { 
+                                  username: '',
+                                  email: '',
+                                  password: '1234'
+                                } 
+                              } unless test.metadata[:has_request]
+      end
+
+
+    end
+  end
 end
